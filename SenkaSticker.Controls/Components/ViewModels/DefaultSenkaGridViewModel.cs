54a609fd03ac6ba0edf1dc8fe7f18198467726e2
heyanshao <2385518847@qq.com>
2025-08-18 17:54:57 +0800

    添加默认窗口

---
+++ SenkaSticker.Controls/Components/ViewModels/DefaultSenkaGridViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia;
+using Avalonia.Interactivity;
+using Avalonia.Styling;
+
+namespace SenkaSticker.Controls.Components.ViewModels
+{
+    public class DefaultSenkaGridViewModel : ViewModelBase
+    {
+
+    }
+}
---
+++ SenkaSticker.Controls/Components/Views/DefaultSenkaGridView.axaml.cs
@@ -0,0 +1,16 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using Avalonia.Styling;
+
+namespace SenkaSticker.Controls.Components.Views
+{
+    public partial class DefaultSenkaGridView : UserControl
+    {
+        public DefaultSenkaGridView()
+        {
+            InitializeComponent();
+        }
+    }
+}
