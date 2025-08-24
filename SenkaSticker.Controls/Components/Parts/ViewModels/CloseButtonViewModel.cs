83879f0cc17bbc643626242a3df98d092247b5d1
Oth1024 <ouka0529@qq.com>
2025-08-24 22:56:39 +0800

    添加Avalonia和类模板

---
+++ SenkaSticker.Controls/Components/Parts/ViewModels/CloseButtonViewModel.cs
@@ -0,0 +1,30 @@
+using Avalonia;
+using Avalonia.Interactivity;
+using Avalonia.Styling;
+using SenkaSticker.Controls.Components.ViewModels;
+
+namespace SenkaSticker.Controls.Components.Parts.ViewModels
+{
+    public class CloseButtonViewModel : ViewModelBase
+    {
+        #region Constructor
+        public CloseButtonViewModel()
+        {
+
+        }
+        #endregion
+
+        #region Events
+        #endregion
+
+        #region Fields
+        #endregion
+
+
+        #region Properties
+        #endregion
+
+        #region Methods
+        #endregion
+    }
+}
